a602b4e1cbb05691764991be0a1ae3ebb091016e
DuyLeVu <levuduy132@gmail.com>
2022-01-18 22:44:14 +0700

    add product-management

--- exercise-service-router/product-manager/karma.conf.js
+++ exercise-service-router/product-manager/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-music-management'),
+      dir: require('path').join(__dirname, './coverage/product-manager'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- exercise-service-router/product-manager/src/app/app.component.spec.ts
+++ exercise-service-router/product-manager/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-product-management'`, () => {
+  it(`should have as title 'product-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-music-management');
+    expect(app.title).toEqual('product-manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-music-management app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('product-manager app is running!');
   });
 });
