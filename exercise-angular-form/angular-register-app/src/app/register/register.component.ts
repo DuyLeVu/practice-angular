6c0be24d4df087e06cb46815d09b86428979205d
DuyLeVu <levuduy132@gmail.com>
2022-01-18 11:20:07 +0700

    done validate form

--- exercise-angular-form/angular-register-app/src/app/register/register.component.ts
+++ exercise-angular-form/angular-register-app/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 import {User} from "../model/user";
 
 let _id = 1;
@@ -12,16 +12,19 @@ let _id = 1;
 export class RegisterComponent implements OnInit {
   registerForm = new FormGroup({
     id: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('',Validators.required),
     country: new FormControl(''),
-    age: new FormControl(''),
-    gender: new FormControl(''),
-    phone: new FormControl('')
+    age: new FormControl('',[Validators.required, Validators.min(16)]),
+    gender: new FormControl('',Validators.required),
+    phone: new FormControl('',[Validators.required, Validators.pattern('(84|0[3|5|7|8|9])+([0-9]{8})\\b')])
   });
 
   users: User[] = [];
+  listCountry = [
+    "Việt Nam", "Trung Quốc", "Nhật Bản"
+  ]
 
   constructor() {
   }
